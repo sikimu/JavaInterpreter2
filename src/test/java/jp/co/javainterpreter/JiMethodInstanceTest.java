3dfcc3c2514c5dc222afaee04093f9515ca122b0
sikimu <sikimu.pg@gmail.com>
2024-03-30 18:18:51 +0900

    どんどん作成中

--- src/test/java/jp/co/javainterpreter/JiMethodInstanceTest.java
+++ src/test/java/jp/co/javainterpreter/JiMethodInstanceTest.java
@@ -1,17 +1,29 @@
 package jp.co.javainterpreter;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JiMethodInstanceTest {
 
-    @Test
-    void run() {
+    @ParameterizedTest
+    @MethodSource("runProvider")
+    void run(String expected, String input) {
 
         JiMethodInstance jiMethodInstance = new JiMethodInstance();
-        jiMethodInstance.codes.add(new String[]{"return", "0"});
+        jiMethodInstance.codes.add(new String[]{"return", input});
         JiObject jiObject = jiMethodInstance.run();
-        assertEquals(jiObject.value(), "0");
+        assertEquals(jiObject.value(), expected);
+    }
+    static Stream<Arguments> runProvider() {
+        return Stream.of(
+                Arguments.of("0", "0")
+        );
     }
 }
