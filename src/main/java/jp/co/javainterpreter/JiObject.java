7294dc5f1efd78e08213c8bc03db36503634b070
sikimu <sikimu.pg@gmail.com>
2024-03-31 00:39:21 +0900

    どんどん作成中

--- src/main/java/jp/co/javainterpreter/JiObject.java
+++ src/main/java/jp/co/javainterpreter/JiObject.java
@@ -2,6 +2,17 @@ package jp.co.javainterpreter;
 
 public sealed interface JiObject {
 
+    static JiObject create(String value) {
+        // intに変換できる場合はint型に変換
+        try {
+            return new JiInt(Integer.parseInt(value));
+        } catch (NumberFormatException ignored) {
+        }
+
+        // 不明エラー
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
     record JiReturn() implements JiObject {
     }
     record JiInt(int value) implements JiObject {
