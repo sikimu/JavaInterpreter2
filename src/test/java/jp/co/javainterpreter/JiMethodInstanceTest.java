debad13951753bb373115c7c2e5bf5d40276a638
sikimu <sikimu.pg@gmail.com>
2024-03-30 18:19:10 +0900

    どんどん作成中

--- src/test/java/jp/co/javainterpreter/JiMethodInstanceTest.java
+++ src/test/java/jp/co/javainterpreter/JiMethodInstanceTest.java
@@ -1,9 +1,7 @@
 package jp.co.javainterpreter;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
