8072657d1743e4a9a98db490c0650526f50d5ba3
sikimu <sikimu.pg@gmail.com>
2024-03-31 22:44:23 +0900

    どんどん作成中

--- src/main/java/jp/co/javainterpreter/JiObject.java
+++ src/main/java/jp/co/javainterpreter/JiObject.java
@@ -3,10 +3,6 @@ package jp.co.javainterpreter;
 public sealed interface JiObject {
 
     static JiObject create(String value) {
-        if("return".equals(value)) {
-            return new JiReturn();
-        }
-
         // intに変換できる場合はint型に変換
         try {
             return new JiInt(Integer.parseInt(value));
@@ -25,8 +21,6 @@ public sealed interface JiObject {
         return jiObjects;
     }
 
-    record JiReturn() implements JiObject {
-    }
     record JiInt(int value) implements JiObject {
     }
 }
