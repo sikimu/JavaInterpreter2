bd93837380172839d732656d9af0bb1332515cd2
sikimu <sikimu.pg@gmail.com>
2024-07-12 00:48:28 +0900

    まーた作り直し

--- src/main/java/jp/co/javainterpreter/JavaInterpreter.java
+++ src/main/java/jp/co/javainterpreter/JavaInterpreter.java
@@ -1,8 +1,5 @@
 package jp.co.javainterpreter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class JavaInterpreter {
 
     /**
@@ -13,6 +10,7 @@ public class JavaInterpreter {
         "&", "|", "^", "~", "<<", ">>", ">>>", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=",
         "<<=", ">>=", ">>>="
     };
+
     /**
      * 区切り文字
      */
@@ -20,97 +18,23 @@ public class JavaInterpreter {
         "(", ")", "{", "}", "[", "]", ".", ",", ";", ":", "=", "?", "@"
     };
 
-    /**
-     * 単語のリスト
-     */
-    private final ArrayList<String> words;
+    public JavaInterpreter(String sourceCode) {
 
-    /**
-     * コンストラクタ
-     * @param lines Javaのコードを1行ずつ格納した配列
-     */
-    public JavaInterpreter(String[] lines) {
-        // コマンドライン引数を解析
-        if (lines == null || lines.length == 0) {
-            throw new IllegalArgumentException("引数が不正です");
-        }
 
-        words = createWordList(lines);
     }
 
     /**
-     * 単語のリストを作成する
-     *
-     * @param lines Javaのコードを1行ずつ格納した配列
-     * @return 単語のリスト
+     * シングルコメントの除去
      */
-    static ArrayList<String> createWordList(String[] lines) {
-        return Arrays.stream(lines)
-            .map(JavaInterpreter::splitByWhitespace)
-            .reduce(new ArrayList<>(), (a, b) -> {
-                a.addAll(b);
-                return a;
-            });
+    static String removeSingleComment(String sourceCode) {
+        return sourceCode.replaceAll("//.*", "");
     }
 
     /**
-     * 演算子と区切り文字で分割する
-     * @param word　文字列
-     * @return 分割された文字列のリスト
+     * マルチコメントの除去
      */
-    public static ArrayList<String> splitByOperatorsAndDelimiters(String word) {
-        ArrayList<String> list = new ArrayList<>();
-        ArrayList<String> operatorsAndDelimiters = getOperatorsAndDelimiters();
-        int start = 0;
-        for (int i = 0; i < word.length(); i++) {
-            for (String operatorOrDelimiter : operatorsAndDelimiters) {
-                if (word.startsWith(operatorOrDelimiter, i)) {
-                    if (start != i) {
-                        list.add(word.substring(start, i));
-                    }
-                    list.add(operatorOrDelimiter);
-                    i += operatorOrDelimiter.length() - 1;
-                    start = i + 1;
-                    break;
-                }
-            }
-        }
-        if (start != word.length()) {
-            list.add(word.substring(start));
-        }
-        return list;
-    }
+    static String removeMultiComment(String sourceCode) {
 
-    /**
-     * 演算子と区切り文字を取得する(長い順にソート)
-     * @return 演算子と区切り文字のリスト
-     */
-    private static ArrayList<String> getOperatorsAndDelimiters() {
-        ArrayList<String> list = new ArrayList<>(Arrays.asList(OPERATORS));
-        list.addAll(Arrays.asList(DELIMITERS));
-        list.sort((a, b) -> b.length() - a.length());
-        return list;
-    }
-
-    /**
-     * 空白文字で分割する
-     * @param word 1行の文字列
-     * @return 分割された文字列のリスト
-     */
-    static ArrayList<String> splitByWhitespace(String word) {
-        ArrayList<String> list = new ArrayList<>();
-        int start = 0;
-        for (int i = 0; i < word.length(); i++) {
-            if (Character.isWhitespace(word.charAt(i))) {
-                if (start != i) {
-                    list.add(word.substring(start, i));
-                }
-                start = i + 1;
-            }
-        }
-        if (start != word.length()) {
-            list.add(word.substring(start));
-        }
-        return list;
+        return sourceCode.replaceAll("/\\*[^*]*\\*+(?:[^/*][^*]*\\*+)*/", "");
     }
 }
--- src/test/java/jp/co/javainterpreter/JavaInterpreterTest.java
+++ src/test/java/jp/co/javainterpreter/JavaInterpreterTest.java
@@ -9,34 +9,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class JavaInterpreterTest {
 
     @Test
-    void splitByOperatorsAndDelimiters(){
-        // / で分割
-        assertEquals(Arrays.asList("a", "/", "b"), JavaInterpreter.splitByOperatorsAndDelimiters("a/b"));
-        // += で分割
-        assertEquals(Arrays.asList("a", "+=", "b"), JavaInterpreter.splitByOperatorsAndDelimiters("a+=b"));
-        // 演算子から始まる場合
-        assertEquals(Arrays.asList("+=", "b"), JavaInterpreter.splitByOperatorsAndDelimiters("+=b"));
-        // 演算子で終わる場合
-        assertEquals(Arrays.asList("a", "+="), JavaInterpreter.splitByOperatorsAndDelimiters("a+="));
+    void testRemoveSingleComment() {
+        assertEquals("a", JavaInterpreter.removeSingleComment("a"));
+        assertEquals("a", JavaInterpreter.removeSingleComment("a//b"));
+        assertEquals("a\nc", JavaInterpreter.removeSingleComment("a//b\nc"));
+        assertEquals("", JavaInterpreter.removeSingleComment("//a"));
     }
 
     @Test
-    void splitByWhitespace(){
-        // 空白で分割
-        assertEquals(Arrays.asList("a", "b", "c"), JavaInterpreter.splitByWhitespace("a b c"));
-        // 空白が連続する場合
-        assertEquals(Arrays.asList("a", "b", "c"), JavaInterpreter.splitByWhitespace("a  b   c"));
-        // 空白で始まる場合
-        assertEquals(Arrays.asList("a", "b", "c"), JavaInterpreter.splitByWhitespace(" a b c"));
-        // 空白で終わる場合
-        assertEquals(Arrays.asList("a", "b", "c"), JavaInterpreter.splitByWhitespace("a b c "));
-        // タブで分割
-        assertEquals(Arrays.asList("a", "b", "c"), JavaInterpreter.splitByWhitespace("a\tb\tc"));
-    }
-
-    @Test
-    void createWordList(){
-        // 単語のリストを作成
-        assertEquals(Arrays.asList("a", "+=", "b", "c", "d"), JavaInterpreter.createWordList(new String[]{"a += b", "c d"}));
+    void testRemoveMultiComment() {
+        assertEquals("a", JavaInterpreter.removeMultiComment("a"));
+        assertEquals("a", JavaInterpreter.removeMultiComment("a/*b*/"));
+        assertEquals("a\nc", JavaInterpreter.removeMultiComment("a/*b*/\nc"));
+        assertEquals("", JavaInterpreter.removeMultiComment("/*a*/"));
+        assertEquals("", JavaInterpreter.removeMultiComment("/*a\nb*/"));
     }
 }
