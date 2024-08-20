76b21ac2718ed0112e26f59fb9c314512d42af9b
sikimu <sikimu.pg@gmail.com>
2024-08-21 01:15:03 +0900

    feat: Add tests for handling whitespace characters in TokenStream
    The code changes include adding tests to handle whitespace characters in the TokenStream class. Two new test methods are added to verify the correct retrieval of the next word when encountering tabs and newlines. This ensures that the TokenStream correctly handles whitespace between tokens.
    
    Resolves: #123

--- src/test/java/jp/co/javainterpreter/stream/TokenStreamTest.java
+++ src/test/java/jp/co/javainterpreter/stream/TokenStreamTest.java
@@ -1,6 +1,5 @@
 package jp.co.javainterpreter.stream;
 
-import jp.co.javainterpreter.token.Token;
 import jp.co.javainterpreter.token.TokenType;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +10,22 @@ public class TokenStreamTest {
     @Test
     void test次の単語を取得する_スペース() {
         TokenStream tokenStream = new TokenStream("public class");
+
+        assertEquals(TokenType.PUBLIC, tokenStream.getNext().type);
+        assertEquals(TokenType.CLASS, tokenStream.getNext().type);
+    }
+
+    @Test
+    void test次の単語を取得する_タブ() {
+        TokenStream tokenStream = new TokenStream("public\tclass");
+
+        assertEquals(TokenType.PUBLIC, tokenStream.getNext().type);
+        assertEquals(TokenType.CLASS, tokenStream.getNext().type);
+    }
+
+    @Test
+    void test次の単語を取得する_改行() {
+        TokenStream tokenStream = new TokenStream("public\n\rclass");
 
         assertEquals(TokenType.PUBLIC, tokenStream.getNext().type);
         assertEquals(TokenType.CLASS, tokenStream.getNext().type);
