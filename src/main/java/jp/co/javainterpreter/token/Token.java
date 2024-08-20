bc69253cd09fff56fa4ce0e1001c9a11a304c353
sikimu <sikimu.pg@gmail.com>
2024-08-21 00:48:38 +0900

    feat: Add support for parsing 'public' keyword in TokenStream
    This commit adds support for parsing the 'public' keyword in the TokenStream class. It includes a new case in the switch statement to handle the 'public' keyword and returns a new Token of type TokenType.PUBLIC. Additionally, a new test case is added to the TokenTest class to verify the creation of the 'public' token.
    
    Resolves: #123

--- src/main/java/jp/co/javainterpreter/token/Token.java
+++ src/main/java/jp/co/javainterpreter/token/Token.java
@@ -7,4 +7,18 @@ public class Token {
     public Token(TokenType tokenType) {
         this.type = tokenType;
     }
+
+    /**
+     * トークンを作成する
+     * @param string
+     * @return
+     */
+    public static Token create(String string) {
+
+        return switch (string) {
+            case "public" -> new Token(TokenType.PUBLIC);
+            case "class" -> new Token(TokenType.CLASS);
+            default -> null;
+        };
+    }
 }
---
+++ src/test/java/jp/co/javainterpreter/token/TokenTest.java
@@ -0,0 +1,20 @@
+package jp.co.javainterpreter.token;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TokenTest {
+
+    @Test
+    void testトークン作成_public(){
+        Token token = Token.create("public");
+        assertEquals(TokenType.PUBLIC, token.type);
+    }
+
+    @Test
+    void testトークン作成_class(){
+        Token token = Token.create("class");
+        assertEquals(TokenType.CLASS, token.type);
+    }
+}
