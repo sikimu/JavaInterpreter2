ae56c2b133e5721e4e4813fbb12fa470eaaf22e6
sikimu <sikimu.pg@gmail.com>
2024-08-31 00:20:42 +0900

    更新しました

--- src/main/java/jp/co/javainterpreter/object/JiParameter.java
+++ src/main/java/jp/co/javainterpreter/object/JiParameter.java
@@ -3,6 +3,13 @@ package jp.co.javainterpreter.object;
 import jp.co.javainterpreter.token.Token;
 
 public class JiParameter {
+
+    public final Token type;
+    public final Token name;
+
     public JiParameter(Token type, Token name) {
+
+        this.type = type;
+        this.name = name;
     }
 }
